1b90a1990f22b2004cb6c14fa04f97b3ab4e375d
nemo <crazy_nemo@126.com>
2016-11-09 14:25:39 +0800

    表结构变更

--- klubb/src/main/webapp/sql/init.sql
+++ klubb/src/main/webapp/sql/init.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2016-11-07 17:20:05
+Date: 2016-11-09 14:24:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -43,12 +43,33 @@ CREATE TABLE `sys_access_log` (
   `URL` varchar(100) DEFAULT NULL COMMENT '访问URL',
   `FUNC_CODE` varchar(30) DEFAULT NULL COMMENT '功能编码',
   `DESCRIPTION` varchar(100) DEFAULT NULL COMMENT '描述',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '访问时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '访问时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='接触日志';
 
 -- ----------------------------
 -- Records of sys_access_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `t_activity`;
+CREATE TABLE `t_activity` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `title` varchar(50) DEFAULT NULL COMMENT '活动名称',
+  `CONTENT` varchar(1000) DEFAULT NULL COMMENT '活动内容',
+  `ACT_TIME` datetime DEFAULT NULL COMMENT '活动时间',
+  `STATUS` tinyint(2) DEFAULT NULL COMMENT '状态，0：草稿，1：正常',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `CREATE_USER_ID` int(11) DEFAULT NULL COMMENT '创建人',
+  `UPDATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `UPDATE_USER_ID` int(11) DEFAULT NULL COMMENT '修改用户ID',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='活动';
+
+-- ----------------------------
+-- Records of t_activity
 -- ----------------------------
 
 -- ----------------------------
@@ -62,31 +83,12 @@ CREATE TABLE `t_act_user` (
   `NAME` varchar(50) DEFAULT NULL COMMENT '姓名',
   `phone` varchar(20) DEFAULT NULL COMMENT '手机号',
   `EMAIL` varchar(50) DEFAULT NULL COMMENT '邮箱',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '报名时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '报名时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='参与活动登记';
 
 -- ----------------------------
 -- Records of t_act_user
--- ----------------------------
-
--- ----------------------------
--- Table structure for t_activity
--- ----------------------------
-DROP TABLE IF EXISTS `t_activity`;
-CREATE TABLE `t_activity` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `title` varchar(50) DEFAULT NULL COMMENT '活动名称',
-  `CONTENT` varchar(1000) DEFAULT NULL COMMENT '活动内容',
-  `ACT_TIME` datetime DEFAULT NULL COMMENT '活动时间',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
-  `STATUS` tinyint(2) DEFAULT NULL COMMENT '状态，0：草稿，1：正常',
-  `CREATE_USER_ID` int(11) DEFAULT NULL COMMENT '创建人',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='活动';
-
--- ----------------------------
--- Records of t_activity
 -- ----------------------------
 
 -- ----------------------------
@@ -114,9 +116,9 @@ DROP TABLE IF EXISTS `t_contact`;
 CREATE TABLE `t_contact` (
   `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `NAME` varchar(50) DEFAULT NULL COMMENT '姓名',
-  `phone` varchar(20) DEFAULT NULL COMMENT '手机号',
+  `PHONE` varchar(20) DEFAULT NULL COMMENT '手机号',
   `CONTENT` varchar(500) DEFAULT NULL COMMENT '内容',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `STATUS` tinyint(2) DEFAULT NULL COMMENT '状态：0：未读，1：已读',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='联系我们';
@@ -135,7 +137,7 @@ CREATE TABLE `t_evaluation` (
   `OBJ_TYPE` tinyint(2) DEFAULT NULL COMMENT '操作对象类型，1：happytimes，2：活动',
   `CONTENT` varchar(300) DEFAULT NULL COMMENT '评价留言',
   `USER_ID` int(11) DEFAULT NULL COMMENT '用户ID',
-  `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `P_ID` int(11) NOT NULL COMMENT '回复那条评论',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评价';
@@ -152,7 +154,7 @@ CREATE TABLE `t_introduce` (
   `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `TYPE` tinyint(2) DEFAULT NULL COMMENT '类型，1：台球厅介绍，2：人员介绍',
   `CONTENT` varchar(1000) DEFAULT NULL COMMENT '内容',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `CREATE_USER_ID` int(11) DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='介绍';
@@ -188,7 +190,7 @@ CREATE TABLE `t_picture` (
   `url` varchar(100) DEFAULT NULL COMMENT '访问url',
   `DESCRIPTION` varchar(200) DEFAULT NULL COMMENT '描述',
   `USER_ID` int(11) DEFAULT NULL COMMENT '归属用户ID',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='happytimes';
 
@@ -210,7 +212,7 @@ CREATE TABLE `t_user` (
   `GENDER` tinyint(2) DEFAULT NULL COMMENT '性别，0：女，1：男',
   `PHONE` varchar(20) DEFAULT NULL COMMENT '手机号',
   `STATUS` tinyint(2) NOT NULL COMMENT '状态，0可用，1不可用',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `CREATE_TIME` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='前台用户';
 
