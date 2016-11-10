97a93cf686f94f11a3bf71cf1bff44316bacecbd
htykbz <htykbz@163.com>
2016-11-10 21:46:33 +0800

    错误版本

--- klubb/src/main/java/com/partner/common/base/BaseConfig.java
+++ klubb/src/main/java/com/partner/common/base/BaseConfig.java
@@ -13,6 +13,7 @@ import com.jfinal.ext.handler.ContextPathHandler;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.render.ViewType;
+import com.partner.busi.back.controller.BackActivityController;
 import com.partner.busi.back.controller.BackContactController;
 import com.partner.busi.back.controller.BackIntroduceController;
 import com.partner.busi.back.controller.BackLoginController;
@@ -57,6 +58,7 @@ public class BaseConfig extends JFinalConfig {
 		me.add("/back/main",BackMainController.class,"/back");
 		me.add("/back/contact",BackContactController.class,"/back");
 		me.add("/back/introduct",BackIntroduceController.class,"/back");
+		me.add("/back/activity",BackActivityController.class,"/back");
 	}
 
 	/*
