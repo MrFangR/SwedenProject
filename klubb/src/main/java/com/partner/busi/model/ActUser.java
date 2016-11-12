ce10604ef68108641f51a64e0fddd0f552941e48
htykbz <htykbz@163.com>
2016-11-13 00:03:05 +0800

    测试

--- klubb/src/main/java/com/partner/busi/model/ActUser.java
+++ klubb/src/main/java/com/partner/busi/model/ActUser.java
@@ -1,6 +1,12 @@
 package com.partner.busi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
 import com.partner.busi.model.base.BaseActUser;
 
 /**
@@ -26,5 +32,25 @@ public class ActUser extends BaseActUser<ActUser> {
 		}else{
 			return true;
 		}
+	}
+	
+	/**
+	 * 根据活动id查询参与用户
+	 */
+	public List<ActUser> getUserByActId(Integer ActId){
+		return find("select s.* from t_act_user s where s.ACT_ID = ?", ActId);
+	}
+
+	public Page<ActUser> findList(String userName, int actID, int pageNum,
+			Integer pagesize) {
+		String select = "select a.*";
+		StringBuilder sql = new StringBuilder(" from t_act_user a where 1=1 ");
+		List<Object> params = new ArrayList<Object>();
+		if (StringUtils.isNotBlank(userName)) {
+			sql.append(" and a.NAME like ? ");
+			params.add("%" + userName + "%");
+		}
+		sql.append("and ACT_ID="+actID+" order by a.CREATE_TIME desc");
+		return paginate(pageNum, pagesize, select, sql.toString(), params.toArray());
 	}
 }
