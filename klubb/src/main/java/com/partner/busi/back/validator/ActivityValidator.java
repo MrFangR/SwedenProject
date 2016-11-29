22fc1f92bb8664691bcbd862aeb1f220f90036ba
htykbz <htykbz@163.com>
2016-11-29 16:42:31 +0800

    后台活动校验

--- klubb/src/main/java/com/partner/busi/back/validator/ActivityValidator.java
+++ klubb/src/main/java/com/partner/busi/back/validator/ActivityValidator.java
@@ -6,18 +6,17 @@ import com.jfinal.validate.Validator;
 public class ActivityValidator extends Validator {
 
 	@Override
-	protected void validate(Controller c) {
-		c.setAttr("rsFlag", false);
-    	c.renderJson();
-	}
-
-	@Override
 	protected void handleError(Controller c) {
-		validateRequiredString("activity.TITLE", "content_msg", "请填写标题");
-		validateDate("activity.ACT_TIME", "content_msg", "请填写正确时间");
-		validateRequiredString("activity.IMG", "content_msg", "请填加图片");
+		validateRequiredString("activity.TITLE", "title_msg", "请填写标题");
+		validateDate("activity.ACT_TIME", "act_time_msg", "请填写正确时间");
+		validateRequiredString("activity.IMG", "img_path_msg", "请填加图片");
 		validateString("activity.ABSTRACT", 1, 200, "abstract_msg", "请输入1~200描述内容");
 		validateString("activity.CONTENT", 1, 200, "content_msg", "请输入1~1000字描述内容");
 	}
 
+	@Override
+	protected void validate(Controller c) {
+		c.setAttr("rsFlag", false);
+    	c.renderJson();
+	}
 }
